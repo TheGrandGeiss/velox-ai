48da50585f9d0080786be135954bd1fa5213d6fd
Ebenebe Chidera <ebenebechidera3@gmail.com>
2026-01-16 02:32:57 +0100

    add plugin

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
 const nextConfig: NextConfig = {
   images: {
@@ -10,6 +11,13 @@ const nextConfig: NextConfig = {
     ],
   },
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
